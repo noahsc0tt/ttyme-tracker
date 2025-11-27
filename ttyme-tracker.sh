7b155e6d13a48bd04261f0abb0da5444a86d0c90
Noah Scott <noah.scott.24@ucl.ac.uk>
2025-11-27 02:23:49 +0000

    start session, --stop and --continue functionality

---
+++ ttyme-tracker.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+CONTINUE=0
+STOP=0
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+      -c|--continue) CONTINUE=1; shift;;
+      -s|--stop) STOP=1; shift;;
+      *) echo "unknown option: $1"; exit 1;;
+    esac
+done
+
+if [[ $CONTINUE -eq 1 ]]; then
+    timew continue
+elif [[ $STOP -eq 1 ]]; then
+    timew stop
+else
+    CATEGORIES=(
+        "Config"
+        "Life"
+        "Relax"
+        "University"
+        "Work"
+        "Waste"
+    )
+    timew start "$(printf "%s\n" "${CATEGORIES[@]}" | fzf --preview-window hidden)"
+fi
