6b09742afcf00b2648f72f9bd2e2dd34244dc752
Noah Scott <noah.scott.24@ucl.ac.uk>
2025-11-27 03:52:23 +0000

    --info for timewarrior summary

--- ttyme-tracker.sh
+++ ttyme-tracker.sh
@@ -1,13 +1,35 @@
 #!/usr/bin/env bash
 
+TAGS=(
+    "Config"
+    "Life"
+    "Relax"
+    "University"
+    "Work"
+    "Waste"
+)
+
 CONTINUE=0
 STOP=0
+INFO=0
+
+function show_help() {
+    echo "Usage: $0 [OPTIONS]"
+    echo
+    echo "Options:"
+    echo "  -c, --continue    Continue the previous activity"
+    echo "  -i, --info        Show this help message"
+    echo "  -s, --stop        Stop the current timewarrior task"
+    echo "  -h, --help        Show this help message"
+}
 
 while [[ $# -gt 0 ]]; do
     case "$1" in
-      -c|--continue) CONTINUE=1; shift;;
-      -s|--stop) STOP=1; shift;;
-      *) echo "unknown option: $1"; exit 1;;
+        -h|--help) show_help; exit 0;;
+        -c|--continue) CONTINUE=1; shift;;
+        -i|--info) INFO=1; shift;;
+        -s|--stop) STOP=1; shift;;
+        *) echo "unknown option: $1"; show_help; exit 1;;
     esac
 done
 
@@ -15,14 +37,10 @@ if [[ $CONTINUE -eq 1 ]]; then
     timew continue
 elif [[ $STOP -eq 1 ]]; then
     timew stop
+elif [[ $INFO -eq 1 ]]; then
+    timew
+    timew summary
 else
-    TAGS=(
-        "Config"
-        "Life"
-        "Relax"
-        "University"
-        "Work"
-        "Waste"
-    )
-    timew start "$(printf "%s\n" "${TAGS[@]}" | fzf --preview-window hidden)"
+    tag="$(printf "%s\n" "${TAGS[@]}" | fzf --preview "timew summary :week {}")"
+    [ -n "$tag" ] && timew start "$tag"
 fi
